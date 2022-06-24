d242b4226d840f016ce95e0c85a74c0c9fc6e1c2
Perpetual <81020670+Perpy-del@users.noreply.github.com>
2022-06-24 04:26:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const aboutMe = {
+  name: "Name: Perpetual Meninwa",
+  height: "Height: 5ft 8inches",
+  country: "Country: Nigeria"
+}
+
+console.log(aboutMe.name); 
+console.log(aboutMe.height);
+console.log(aboutMe.country)
